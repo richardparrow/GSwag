9ded0844b404681f1a3b8d3f0bc728b574f6cc44
Riccardo Parviero <richardparrow@MacBook-Pro-di-Riccardo-2.local>
2018-09-17 22:18:03 +0200

    Messaggio importantissimo di importanza nazionale alla nazione.

---
+++ yo!.R
@@ -0,0 +1,13 @@
+
+devtools::install_github("brooke-watson/BRRR")
+library(BRRR)
+
+# Benvenuti su GSwag!
+# Speriamo di swaggare anche sulla leaderboard!
+
+# Perchè ricordatevi che...
+skrrrahh("khaled2")
+
+# Ma ricordatevi che...
+skrrrahh("khaled3")
+
