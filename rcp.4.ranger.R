768ca14321e844c69d2792b47221f4b21e0f0676
Riccardo Parviero <riccarpa@horus.uio.no>
2018-09-19 17:54:01 +0200

    Forestina randomella tanto per.

--- rcp.4.ranger.R
+++ rcp.4.ranger.R
@@ -11,7 +11,26 @@ print(system.time(
   
 rfModel <- ranger(dependent.variable.name = "transactionRevenue",
                   data = rangerXTrain,
-                  num.trees = 1000,
-                  mtry = 30)
+                  num.trees = 100,
+                  mtry = NULL, # defaults to sqrt(p)
+                  importance = "impurity") 
 
-)) # 90 seconds!
+)) # 2h ca.
+
+
+
+# plotting variable importance or die trying
+rangerImp = importance(rfModel)
+
+library(tidyverse)
+varImp = cbind(names(rangerImp), as.numeric(rangerImp)) %>% 
+  as.tibble() %>%
+  mutate(varName = V1 %>% as.factor(),
+         varImp = V2 %>% as.numeric() %>% scale()) %>%
+  select(-V1, -V2)
+
+orderImp = varImp[order(varImp$varImp, decreasing = T), ]
+orderImp$varName = factor(orderImp$varName, levels = orderImp$varName[order(orderImp$varImp)])
+
+library(ggplot2)
+ggplot(orderImp[1:20, ]) + aes(x = varName, y = varImp, fill = varImp) + geom_bar(stat = "identity") + coord_flip()
