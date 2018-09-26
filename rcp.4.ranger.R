b7a56e5d2a8188bf8d1829e88e3d20736afc4aca
Riccardo Parviero <riccarpa@math.uio.no>
2018-09-26 09:25:16 +0200

    Provette con le foreste(tte).

--- rcp.4.ranger.R
+++ rcp.4.ranger.R
@@ -7,20 +7,54 @@ load("modelMatrixTrain_ranger.RData")
 
 
 # Random Forest with ranger!
+
+# model 1: num.trees = 1000; mtry = 35 ~ sqrt(p)  ---  2h 40m (num.threads = 6)
 print(system.time(
   
-rfModel <- ranger(dependent.variable.name = "transactionRevenue",
-                  data = rangerXTrain,
-                  num.trees = 10,
-                  mtry = 500, # defaults to sqrt(p)
-                  importance = "impurity") 
+rfOne <- ranger(dependent.variable.name = "transactionRevenue",
+                data = rangerXTrain,
+                num.trees = 1000,
+                mtry = NULL, # defaults to sqrt(p)
+                importance = "impurity",
+                num.threads = 6) 
 
+))
+
+# model 2: num.trees = 1000; mtry = 100 > sqrt(p)  ---  6h (num.threads = 6)
+print(system.time(
+  
+rfTwo <- ranger(dependent.variable.name = "transactionRevenue",
+                data = rangerXTrain,
+                num.trees = 1000,
+                mtry = 100, # > sqrt(p)
+                importance = "impurity",
+                num.threads = 6)  
+  
+))
+
+# model 3: num.trees = 1000; mtry = 10 < sqrt(p)  ---  1h 6m (num.threads = 6)
+print(system.time(
+  
+rfThr <- ranger(dependent.variable.name = "transactionRevenue",
+                data = rangerXTrain,
+                num.trees = 1000,
+                mtry = 10, # < sqrt(p)
+                importance = "impurity",
+                num.threads = 6)  
+  
 ))
 
 
 
+# OOB Prediction RMSEs:
+# mtry =  10:  1.89
+#         35:  1.75
+#        100:  1.65  ---  Eureka?
+
+
+
 # plotting variable importance or die trying
-rangerImp = importance(rfModel)
+rangerImp = importance(rfTwo)
 
 library(tidyverse)
 varImp = cbind(names(rangerImp), as.numeric(rangerImp)) %>% 
@@ -40,9 +74,21 @@ ggplot(orderImp[1:firstK, ]) + aes(x = varName, y = varImp, fill = varImp) + geo
 
 
 # vediamo un po' come fitta
-rfFitted = predict(rfModel, rangerXTrain)
+rfFitted = predict(rfTwo, rangerXTrain)
 plot(density(rangerXTrain[, "transactionRevenue"], from = 0, bw = 1))
 lines(density(rfFitted$predictions, from = 0, bw = 1), col = "red")
 
+
+
+# Submitting!
+load("modelMatrixTest_ranger.RData")
+rfPreds = predict(rfTwo, rangerXTest)$predictions
+
+library(dplyr)
+predSub = cbind(testIds, rfPreds) %>% as.tibble() %>%
+  group_by(fullVisitorId) %>%
+  summarise(PredictedLogRevenue = log1p(sum(expm1(rfPreds))))
+
+readr::write_csv(predSub, path = paste0(getwd(), "/sub_rf_mtry100.csv"))
 
 
